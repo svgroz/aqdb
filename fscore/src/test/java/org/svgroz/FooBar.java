23a57c91f7ea6ab8d71bee238a083b6e6301adb8
Simon Grozovsky <svgroz@outlook.com>
2021-08-17 23:45:49 +0300

    Initial commit

---
+++ fscore/src/test/java/org/svgroz/FooBar.java
@@ -0,0 +1,13 @@
+package org.svgroz;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Simon Grozovsky svgroz@outlook.com
+ */
+public class FooBar {
+    @Test
+    public void test1() {
+
+    }
+}
